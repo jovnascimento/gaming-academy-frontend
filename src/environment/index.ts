3f79eb63711c0b58cde7e423fde54636d9c0468d
Cristiano Costa <cristiano@cristianocosta.me>
2022-07-13 18:32:07 -0300

    fix: env var

--- src/environment/index.ts
+++ src/environment/index.ts
@@ -1,7 +1,7 @@
 const env = {
   API_URL:
     process.env.NODE_ENV === "production"
-      ? "http://157.245.216.89:5001"
+      ? "http://157.245.216.89:5001/api/v1"
       : "http://localhost:4000/api/v1",
 };
 
